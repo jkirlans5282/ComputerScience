f73bff87b490f1a9fff587878e740e3df875e78a
jkirlans5282 <jacobkirlanstout@gmail.com>
2015-06-05 19:06:01 -0600

    Linux drivers, practice

---
+++ C/HelloWorld.c
@@ -0,0 +1,11 @@
+#include <linux/module.h> 
+#include <linux/kernal.h>
+int init_module(void)
+{
+	printk(KERN_INFO "Hello World 1.\n");
+	return 0;
+}
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbye world 1.\n");
+}
